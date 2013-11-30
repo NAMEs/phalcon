35e1bc11212b23f3e76173cb1964ce63c7dcc0b8
Juan Pablo Addeo <jpaddeo@gmail.com>
2013-11-30 13:54:56 -0300

    Arreglos de alineacion (format document)

--- public/index.php
+++ public/index.php
@@ -33,108 +33,108 @@ try {
      */
     $di->set('dispatcher', function() use ($di) {
 
-                $eventsManager = $di->getShared('eventsManager');
+        $eventsManager = $di->getShared('eventsManager');
 
-                $security = new Security($di);
+        $security = new Security($di);
 
-                /**
-                 * We listen for events in the dispatcher using the Security plugin
-                 */
-                $eventsManager->attach('dispatch', $security);
+        /**
+         * We listen for events in the dispatcher using the Security plugin
+         */
+        $eventsManager->attach('dispatch', $security);
 
-                $dispatcher = new Phalcon\Mvc\Dispatcher();
-                $dispatcher->setEventsManager($eventsManager);
+        $dispatcher = new Phalcon\Mvc\Dispatcher();
+        $dispatcher->setEventsManager($eventsManager);
 
-                return $dispatcher;
-            });
+        return $dispatcher;
+    });
 
     /**
      * The URL component is used to generate all kind of urls in the application
      */
     $di->set('url', function() use ($config) {
-                $url = new \Phalcon\Mvc\Url();
-                $url->setBaseUri($config->application->baseUri);
-                return $url;
-            });
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri($config->application->baseUri);
+        return $url;
+    });
 
 
     $di->set('view', function() use ($config) {
 
-                $view = new \Phalcon\Mvc\View();
+        $view = new \Phalcon\Mvc\View();
 
-                $view->setViewsDir(__DIR__ . $config->application->viewsDir);
+        $view->setViewsDir(__DIR__ . $config->application->viewsDir);
 
-                $view->registerEngines(array(
-                    ".volt" => 'volt'
-                ));
+        $view->registerEngines(array(
+            ".volt" => 'volt'
+        ));
 
-                return $view;
-            });
+        return $view;
+    });
 
     /**
      * Setting up volt
      */
     $di->set('volt', function($view, $di) {
 
-                $volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
+        $volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
 
-                $volt->setOptions(array(
-                    "compiledPath" => "../cache/volt/"
-                ));
+        $volt->setOptions(array(
+            "compiledPath" => "../cache/volt/"
+        ));
 
-                return $volt;
-            }, true);
+        return $volt;
+    }, true);
 
     /**
      * Database connection is created based in the parameters defined in the configuration file
      */
     $di->set('db', function() use ($config) {
-                return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
-                    "host" => $config->database->host,
-                    "username" => $config->database->username,
-                    "password" => $config->database->password,
-                    "dbname" => $config->database->name
-                ));
-            });
+        return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+            "host" => $config->database->host,
+            "username" => $config->database->username,
+            "password" => $config->database->password,
+            "dbname" => $config->database->name
+        ));
+    });
 
     /**
      * If the configuration specify the use of metadata adapter use it or use memory otherwise
      */
     $di->set('modelsMetadata', function() use ($config) {
-                if (isset($config->models->metadata)) {
-                    $metaDataConfig = $config->models->metadata;
-                    $metadataAdapter = 'Phalcon\Mvc\Model\Metadata\\' . $metaDataConfig->adapter;
-                    return new $metadataAdapter();
-                }
-                return new Phalcon\Mvc\Model\Metadata\Memory();
-            });
+        if (isset($config->models->metadata)) {
+            $metaDataConfig = $config->models->metadata;
+            $metadataAdapter = 'Phalcon\Mvc\Model\Metadata\\' . $metaDataConfig->adapter;
+            return new $metadataAdapter();
+        }
+        return new Phalcon\Mvc\Model\Metadata\Memory();
+    });
 
     /**
      * Start the session the first time some component request the session service
      */
     $di->set('session', function() {
-                $session = new Phalcon\Session\Adapter\Files();
-                $session->start();
-                return $session;
-            });
+        $session = new Phalcon\Session\Adapter\Files();
+        $session->start();
+        return $session;
+    });
 
     /**
      * Register the flash service with custom CSS classes
      */
     $di->set('flash', function() {
-                return new Phalcon\Flash\Direct(array(
-                    'error' => 'alert alert-error',
-                    'success' => 'alert alert-success',
-                    'notice' => 'alert alert-info',
-                ));
-            });
+        return new Phalcon\Flash\Direct(array(
+            'error' => 'alert alert-error',
+            'success' => 'alert alert-success',
+            'notice' => 'alert alert-info',
+        ));
+    });
 
     /**
      * Register a user component
      */
     $di->set('elements', function() {
-                return new Elements();
-            });
+        return new Elements();
+    });
 
     $application = new \Phalcon\Mvc\Application();
     $application->setDI($di);
