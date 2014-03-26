ec8011874bee5a2a6f11d0910775fe1f3702bd91
Juan Pablo Addeo <jpaddeo@gmail.com>
2014-03-26 17:50:34 -0300

    cambios

--- framework/database.sql
+++ framework/database.sql
@@ -3,70 +3,77 @@ SET FOREIGN_KEY_CHECKS = 0;
 /* FRAMEWORK */
 DROP TABLE IF EXISTS usuarios;
 CREATE TABLE usuarios (
-    usuario_id INT NOT NULL AUTO_INCREMENT,
-    usuario_rol INT NOT NULL,
-    usuario_nombre VARCHAR(255) NOT NULL,
-    usuario_login VARCHAR(255) NOT NULL UNIQUE,
-    -- usuario_email VARCHAR(255) NOT NULL UNIQUE,
-    usuario_password VARCHAR(255) NOT NULL,
-    PRIMARY KEY(usuario_id),
-    FOREIGN KEY (usuario_rol) REFERENCES roles (rol_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    rol_id INT NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (rol_id) REFERENCES roles (id)
 );
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-    rol_id INT NOT NULL AUTO_INCREMENT,
-    rol_nombre VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY(rol_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
 );
 
 DROP TABLE IF EXISTS permisos;
 CREATE TABLE permisos (
-    permiso_id INT NOT NULL AUTO_INCREMENT,
-    permiso_nombre VARCHAR(255) NOT NULL,
-    permiso_descripcion TEXT,
-    permiso_controller VARCHAR(255) NOT NULL,
-    permiso_accion VARCHAR(255) NOT NULL,
-    PRIMARY KEY(permiso_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    controller VARCHAR(255) NOT NULL,
+    accion VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 DROP TABLE IF EXISTS roles_permisos;
 CREATE TABLE roles_permisos (
-    rol_permiso_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     rol_id INT NOT NULL,
     permiso_id INT NOT NULL,
-    PRIMARY KEY(rol_permiso_id),
-    FOREIGN KEY (rol_id) REFERENCES roles (rol_id),
-    FOREIGN KEY (permiso_id) REFERENCES permisos (permiso_id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (rol_id) REFERENCES roles (id),
+    FOREIGN KEY (permiso_id) REFERENCES permisos (id)
 );
 
 INSERT INTO roles
-(rol_nombre)
+(nombre)
 VALUES
 ('ADMINISTRADOR');
 
 INSERT INTO usuarios
-(usuario_rol, usuario_nombre, usuario_login, usuario_password)
+(rol_id, nombre, login, password)
 VALUES
-(1, 'Administrador General', 'admin',MD5('Admin1234'));
+(1, 'Administrador General', 'admin',SHA1('asd123'));
 
 INSERT INTO permisos
-(permiso_nombre, permiso_descripcion, permiso_controller, permiso_accion)
+(nombre, descripcion, controller, accion)
 VALUES
-('Proveedores Inicio','Permiso que habilita la pantalla inicial de Proveedores','proveedores','index'),
-('Proveedores Busqueda','Permiso que habilita la busqueda de Proveedores','proveedores','search'),
-('Proveedores Nuevo','Permiso que habilita el formulario de Crear Proveedores','proveedores','new'),
-('Proveedores Editar','Permiso que habilita Editar un Proveedor','proveedores','edit'),
-('Proveedores Grabar','Permiso que habilita Grabar un Proveedor','proveedores','save'),
-('Proveedores Crear','Permiso que habilita Crear un Proveedor','proveedores','create'),
-('Proveedores Borrar','Permiso que habilita Borrar un Proveedor','proveedores','delete'),
-('Direcciones Inicio','Permiso que habilita la pantalla inciial de Direcciones','direcciones','index'),
-('Direcciones Busqueda','Permiso que habilita la busqueda de Direcciones','direcciones','search'),
-('Direcciones Nuevo','Permiso que habilita el formulario de Crear Direccion','direcciones','new'),
-('Direcciones Editar','Permiso que habilita Editar una Direccion','direcciones','edit'),
-('Direcciones Grabar','Permiso que habilita Grabar una Direccion','direcciones','save'),
-('Direcciones Crear','Permiso que habilita Crear una Direccion','direcciones','create'),
-('Direcciones Borrar','Permiso que habilita Borrar una Direccion','direcciones','delete');
+('Permisos Inicio','Permiso que habilita la pantalla inicial de Permisos','permisos','index'),
+('Permisos Busqueda','Permiso que habilita la busqueda de Permisos','permisos','search'),
+('Permisos Nuevo','Permiso que habilita el formulario de Crear Permisos','permisos','new'),
+('Permisos Editar','Permiso que habilita Editar un Permiso','permisos','edit'),
+('Permisos Grabar','Permiso que habilita Grabar un Permiso','permisos','save'),
+('Permisos Crear','Permiso que habilita Crear un Permiso','permisos','create'),
+('Permisos Borrar','Permiso que habilita Borrar un Permiso','permisos','delete'),
+('Roles Inicio','Permiso que habilita la pantalla inciial de Roles','roles','index'),
+('Roles Busqueda','Permiso que habilita la busqueda de Roles','roles','search'),
+('Roles Nuevo','Permiso que habilita el formulario de Crear Rol','roles','new'),
+('Roles Editar','Permiso que habilita Editar una Rol','roles','edit'),
+('Roles Grabar','Permiso que habilita Grabar una Rol','roles','save'),
+('Roles Crear','Permiso que habilita Crear una Rol','roles','create'),
+('Roles Borrar','Permiso que habilita Borrar una Rol','roles','delete'),
+('Usuarios Inicio','Permiso que habilita la pantalla inciial de Usuarios','usuarios','index'),
+('Usuarios Busqueda','Permiso que habilita la busqueda de Usuarios','usuarios','search'),
+('Usuarios Nuevo','Permiso que habilita el formulario de Crear Usuario','usuarios','new'),
+('Usuarios Editar','Permiso que habilita Editar una Usuario','usuarios','edit'),
+('Usuarios Grabar','Permiso que habilita Grabar una Usuario','usuarios','save'),
+('Usuarios Crear','Permiso que habilita Crear una Usuario','usuarios','create'),
+('Usuarios Borrar','Permiso que habilita Borrar una Usuario','usuarios','delete');
 
 INSERT INTO roles_permisos
 (rol_id, permiso_id)
@@ -85,4 +92,9 @@ VALUES
 (1, 12),
 (1, 13),
 (1, 14),
-(1, 15);
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
+(1, 19),
+(1, 20);
